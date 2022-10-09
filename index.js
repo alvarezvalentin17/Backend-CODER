e8589381469e73746a15ef342b5cff74fd2e8e51
alvarezvalentin17 <valen.alvarez27@icloud.com>
2022-10-08 22:56:30 -0300

    desafio 2 terminado

--- index.js
+++ index.js
@@ -1,84 +1,80 @@
 const fs = require('fs');
 const path = require('path');
 
-const nameFile = 'products.json';
 class Contenedor {
-    
-    constructor(nameFile){
-
+    constructor(archivo){
+        this.nameFile = archivo;
     }
 
 
-}
+    async readFile () {
+            const dato = await fs.promises.readFile(this.nameFile, 'utf-8');
+            return JSON.parse(dato);
+        }
 
+    async main() {
+        const products = await this.readFile();
+        return products
+    }
 
-
-
-
-
-const readFile = async ()=> {
-    const dato = await fs.promises.readFile(nameFile, 'utf-8');
-    return JSON.parse(dato);
-}
-
-const getAll = async () => {
-const products = await readFile();
-return products
-}
-
-const main = async () => {
-        const data = await getAll();
+    async getAll () {
+        const data = await  this.main();
         console.log(data)
-} 
+    } 
 
-const getById = (id)=> {
-    const products = readFile();
-    const indice = products.findIndex((aProducts)=> aProducts.id == id);
+    async getById (id) {
+        const products = await this.readFile();
+        const indice = products.findIndex((aProducts)=> aProducts.id == id);
 
-    if (indice < 0){
-        throw new Error ('El producto no existe');
+        if (indice < 0){
+            throw new Error ('El producto no existe');
+        }
+
+        return console.log(products[indice]);
     }
 
-    return products[indice];
-}
-
-const saveProducts = async (products)=> {
-    const data = JSON.stringify(products, null, '\t')
-    await fs.promises.writeFile(nameFile, data)
-}
-
-const save = (data)=> {
-    if (!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('Datos invalidos')
-    
-    const products = readFile();
-    const product = {
-        title: data.title,
-        price: data.price,
-        id: products[products.length - 1].id + 1
-    }
-    
-    products.push(product)
-    saveProducts(products)
-    
-}
-
-const deleteAll = () => {
-    saveProducts([])
-}
-
-const deleteById = (id)=> {
-    const products = readFile();
-    const indice = products.findIndex((aProducts)=> aProducts.id == id);
-
-    if(indice < 0){
-        return;
+    async saveProducts (products) {
+        const data = JSON.stringify(products, null, '\t')
+        await fs.promises.writeFile(this.nameFile, data)
     }
 
-    products.splice(indice,1)
-    saveProducts(products)
+    async save (data) {
+        if (!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('Datos invalidos')
+        
+        const products =await this.readFile();
+        const product = {
+            title: data.title,
+            price: data.price,
+            id: products[products.length - 1].id + 1
+        }
+        
+        products.push(product)
+        this.saveProducts(products)
+        
+    }
+
+    async deleteAll () {
+        await this.saveProducts([])
+    }
+
+    async deleteById (id) {
+        const products = await this.readFile();
+        const indice = products.findIndex((aProducts)=> aProducts.id == id);
+
+        if(indice < 0){
+            return;
+        }
+
+        products.splice(indice,1)
+        await saveProducts(products)
+    }
+
 }
 
-console.log(main())
-
-
-
+const contenedor = new Contenedor('products.json');
+contenedor.save({
+    title: 'RAM',
+    price: 34.5
+});
+contenedor.getById(3);
+contenedor.getAll();
