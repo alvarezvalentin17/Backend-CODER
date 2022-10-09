83f31e33f0c208a6e53e5d975fe693137eb217cc
alvarezvalentin17 <valen.alvarez27@icloud.com>
2022-10-08 22:09:48 -0300

    empezando a pasarlo a clases

--- index.js
+++ index.js
@@ -2,21 +2,37 @@ const fs = require('fs');
 const path = require('path');
 
 const nameFile = 'products.json';
+class Contenedor {
+    
+    constructor(nameFile){
 
-const readFile = ()=> {
-    const dato = fs.readFileSync(nameFile, 'utf-8');
-    return JSON.parse(dato);
+    }
+
 
 }
 
-const getAll = () => {
-const products = readFile()
+
+
+
+
+
+const readFile = async ()=> {
+    const dato = await fs.promises.readFile(nameFile, 'utf-8');
+    return JSON.parse(dato);
+}
+
+const getAll = async () => {
+const products = await readFile();
 return products
 }
 
+const main = async () => {
+        const data = await getAll();
+        console.log(data)
+} 
+
 const getById = (id)=> {
     const products = readFile();
-
     const indice = products.findIndex((aProducts)=> aProducts.id == id);
 
     if (indice < 0){
@@ -26,17 +42,15 @@ const getById = (id)=> {
     return products[indice];
 }
 
-const saveProducts = (products)=> {
+const saveProducts = async (products)=> {
     const data = JSON.stringify(products, null, '\t')
-    fs.writeFileSync(nameFile, data)
+    await fs.promises.writeFile(nameFile, data)
 }
 
 const save = (data)=> {
-    
     if (!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('Datos invalidos')
     
     const products = readFile();
-    
     const product = {
         title: data.title,
         price: data.price,
@@ -55,14 +69,16 @@ const deleteAll = () => {
 const deleteById = (id)=> {
     const products = readFile();
     const indice = products.findIndex((aProducts)=> aProducts.id == id);
+
     if(indice < 0){
         return;
     }
+
     products.splice(indice,1)
     saveProducts(products)
-
-
 }
+
+console.log(main())
 
 
 
